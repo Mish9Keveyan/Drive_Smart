2dd162a7dd69304a2b2aa814056b1c3d9be2acf2
Keveyan <Mishakeveyan831@gmail.com>
2023-03-29 17:04:13 +0400

    Start commit

--- app/src/main/java/com/example/learntodrive/MainActivity.java
+++ app/src/main/java/com/example/learntodrive/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         TextView tvUsername = findViewById(R.id.tvUsernameHome);
         CardView cvStartTest = findViewById(R.id.cvStartTest);
 
